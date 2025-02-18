70c17978a24b916d81a00a1cb4a7f1c5fe7f29cc
mohamed_ashraf <m.ashraf@bayanatz.com>
2025-02-18 23:15:08 +0200

    list

--- lib/project.dart
+++ lib/project.dart
@@ -1,31 +1,6 @@
 void main(){
-  String name = '';
-  print(name.indexOf(''));
+  List random = ['a','m','s'];
+  random.addAll(['a','sd']);
+  print(random);
 }
 
-chartAt(String word , int index){
-  try{
-    return word[index];
-  } on RangeError{
-    throw Exception(RangeError);
-  }
-}
-
-
-isEmpty(String word){
-  stringLength(word) == 0 ? true : false;
-}
-
-// fun to get lwnght of string
-stringLength(String word){
-  int i = 0;
-  while(true){
-    try{
-      var char = word[i];
-      i++;
-    }catch (e){
-      break;
-    }
-  }
-  return i;
-}
