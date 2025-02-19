3ab591e54ff30e4722efed92fe259f684f05a965
mohamed_ashraf <moashrafasd12@gmail.com>
2025-02-19 08:55:59 +0200

    filter list by where

--- lib/project.dart
+++ lib/project.dart
@@ -1,18 +1,13 @@
 
 
 
+
 void main(){
 
   List number = [2,10,20,30,40,50,60,70,80,90,100];
   List number2 = [];
 
-for(int i = 0 ; i < number.length ; i++){
-  if(number[i] >= 20){
-
-    number2.add(number[i] * 2);
-  }
-
-}
+ number2 = number.where((e) => e > 40).toList();
 
   print(number2);
 
