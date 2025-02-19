6b3041a2b8f9dd9f90fe15661f61a45ef72c2cc5
mohamed_ashraf <m.ashraf@bayanatz.com>
2025-02-19 08:20:47 +0200

    list0

--- lib/project.dart
+++ lib/project.dart
@@ -1,6 +1,13 @@
+
+
+
 void main(){
-  List random = ['a','m','s'];
-  random.addAll(['a','sd']);
-  print(random);
+  List random = ['mohamed','ahmed','ali'];
+  List random2 = ['o','d','g'];
+ List random3 = [...random,...random2];
+
+print(random3);
+
+
 }
 
