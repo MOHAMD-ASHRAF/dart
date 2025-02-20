92360fba02c3c9940a850dde9a22e0e498b67b87
mohamed_ashraf <moashrafasd12@gmail.com>
2025-02-20 13:59:26 +0200

    set

--- lib/project.dart
+++ lib/project.dart
@@ -4,10 +4,9 @@
 
 void main(){
 
-  List number = [2,10,20,30,40,50,60,70,80,90,100];
-  List number2 = [];
-
- number2 = number.where((e) => e > 40).toList();
+  Set student ={'mo', 'ashraf' , 'ali '};
+  student.add('value');
+  print(student);
 
 
 }
